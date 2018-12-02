4503ca8ec0161fed673b946db18deea4b52620cf
10Nathan <35432638+10Nathan@users.noreply.github.com>
2018-12-02 17:30:48 -0600

    Work on Trips

--- WeatherTutorial/WeatherTutorial/TravelPage.xaml.cs
+++ WeatherTutorial/WeatherTutorial/TravelPage.xaml.cs
@@ -20,11 +20,43 @@ namespace WeatherTutorial
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    
     public sealed partial class TravelPage : Page
     {
         public TravelPage()
         {
             this.InitializeComponent();
+        }
+
+        static int count = 1;
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Trip_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton newButton = new RadioButton();
+            newButton.Width = 5000;
+
+            newButton.Click += SubscribeButton_Click;
+
+            newButton.Content = "Trip: " + count;
+            count++;
+
+            listView.Items.Add(newButton);
+        }
+
+
+        private async void SubscribeButton_Click(object sender, RoutedEventArgs e)
+        {
+            int a = 5;
+        }
+
+        private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            int b = 5;
         }
     }
 }
